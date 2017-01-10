e3b5e5a1186d93067abde554b8282c71d528bf30
KFranciszek <krzysztof.zernicki@gmail.com>
2017-01-10 22:42:50 +0100

    Update string.py

--- string.py
+++ string.py
@@ -1,3 +1,11 @@
-name = "Krzysztof"
+name = "Franek"
 print (name)
+print (name+ " to super gość")
+#
+print(5*5/3) 
+#
+about = "Lubię gotować i chodzić po lesie ponieważ jestem szalony"
+print (about)
+print (about[0:42])
+print (len(about))
 #

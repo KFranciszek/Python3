aff045ae5d082658bd4a5e3d8ca1384b80a470d2
KFranciszek <krzysztof.zernicki@gmail.com>
2017-01-11 21:22:24 +0100

    Create if_else_1.py

---
+++ if_else_1.py
@@ -0,0 +1,9 @@
+ugly = float(input("Podaj liczbę"))
+if ugly  % 2 == 0:
+    print("ładna liczba")
+elif ugly % 3 == 0:
+    print("ładna liczba")
+elif ugly % 5 == 0:
+    print("ładna liczba")
+else:
+    print ("brzydko")

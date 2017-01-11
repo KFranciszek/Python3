dc6edbcf888b2836f93e5bcf00cf1d3d29c9d657
KFranciszek <krzysztof.zernicki@gmail.com>
2017-01-11 21:31:41 +0100

    Update if_else_1.py

--- if_else_1.py
+++ if_else_1.py
@@ -7,3 +7,13 @@ elif ugly % 5 == 0:
     print("ładna liczba")
 else:
     print ("brzydko")
+####
+about = ("Lubię gotować i chodzić po lesie") 
+if len (about) > 50:
+    print ("Dozwolona liczba znaków to 50")
+else:
+    print("Możesz przejść dalej")
+
+    
+    
+    

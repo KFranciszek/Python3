7cfc705b6e7c56ff5f1272dd476682b101085de7
KFranciszek <krzysztof.zernicki@gmail.com>
2017-01-11 20:17:21 +0100

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+slownik1 = {}
+print (slownik1)
+#
+slownik2 = {'gra':'Mass Efect'}
+print (slownik2)
+#
+slownik3 = {'gra1': 'Andromeda', 'gra2': 'gothic'}
+print = (slownik3['gra2'])
+#
+slownik4 = {'gra1': 'Andromeda', 'gra2': 'gothic', 7: 'gra3'}
+print(slownik4 [7])
+#
+slownik6 = {'gra1': 'Andromeda', 'gra2': 'gothic'}
+slownik6['gra1'] = 'Mass Efect'
+print (slownik6)
+#

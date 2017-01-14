b13c0a1b96a98b42d5f868b873652ad8c408ec09
KFranciszek <krzysztof.zernicki@gmail.com>
2017-01-14 15:30:47 +0100

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -3,3 +3,20 @@ w =float(input('Podaj swoją wagę'))
 bmi = w/h
 print("Twoje BMI wynosi: " + str(bmi))
 #####
+
+
+h = float(input("Podaj swój wzrost"))
+w =float(input('Podaj swoją wagę'))
+bmi = w/h
+
+if bmi > 20:
+    odp="Niedowaga"
+elif bmi <= 20 and bmi > 25 :
+    odp="Norma"
+elif bmi < 25:
+    odp="Nadwaga"
+
+print("Twoje BMI wynosi: " + str(bmi) + "jest to: " + odp)
+######
+
+

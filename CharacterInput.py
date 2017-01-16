9128d99da4755dcc7d855e4228aa84ba2d9b2f99
KFranciszek <krzysztof.zernicki@gmail.com>
2017-01-16 20:13:41 +0100

    Update CharacterInput.py

--- CharacterInput.py
+++ CharacterInput.py
@@ -1,6 +1,7 @@
 #task from site http://www.practicepython.org/exercise/2014/01/29/01-character-input.html
 #My solution
 
+name = input('What is your name: ')
 age = float(input('How old are you'))
 math = int(2017 + (100 - age))
-print("You will have 100 years in: " + str(math))
+print(name + " you will have 100 years in: " + str(math))

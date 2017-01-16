ac66178064240506e05a88f41d51cc72bef4b000
KFranciszek <krzysztof.zernicki@gmail.com>
2017-01-16 20:44:49 +0100

    Create OddOrEven.py

---
+++ OddOrEven.py
@@ -0,0 +1,8 @@
+#Task from  http://www.practicepython.org/exercise/2014/02/05/02-odd-or-even.html
+#My solution
+
+number = int(input('Give a number'))
+if number % 2 == 0:
+    print(str(number)  + ' number is even')
+else:
+    print(str(number) + ' number is odd')

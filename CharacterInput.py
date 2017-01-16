003234b45dab71b8326216261d637138bb7e89c8
KFranciszek <krzysztof.zernicki@gmail.com>
2017-01-16 20:03:48 +0100

    Create CharacterInput.py

---
+++ CharacterInput.py
@@ -0,0 +1,6 @@
+#task from site http://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+#My solution
+
+age = float(input('How old are you'))
+math = int(2017 + (100 - age))
+print("You will have 100 years in: " + str(math))

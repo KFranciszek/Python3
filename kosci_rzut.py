50c63259f421acab3d9362a3340026fd4f14ce6b
KFranciszek <krzysztof.zernicki@gmail.com>
2017-01-21 11:18:43 +0100

    Create kosci_rzut.py

---
+++ kosci_rzut.py
@@ -0,0 +1,11 @@
+import random
+kosc = random.randint(1,6)
+kosc2 =random.randint(1,6)
+if kosc > kosc2:
+    print ("Gracz1: " +  str(kosc), "Gracz2: " + str(kosc2), ' Gracz 1 wygywa')
+elif kosc < kosc2:
+    print("Gracz1: " +  str(kosc), "Gracz2: " + str(kosc2), 'Gracz 2 wygywa')
+elif kosc == kosc2:
+    print("Gracz1: " +  str(kosc), "Gracz2: " + str(kosc2), "Remis")
+else:
+    print("Brak wyniku")

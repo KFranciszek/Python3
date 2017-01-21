044d7b7c45cedaa1f567e68cd75af35274e9caf2
KFranciszek <krzysztof.zernicki@gmail.com>
2017-01-21 14:24:01 +0100

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -7,4 +7,8 @@ a = 17%7
 z = a*(a+3)
 print(z)
 ###########
-
+bok1 = float(input('Podaj wysokość'))
+bok2 = float(input('Podaj długość'))
+t =  bok1*bok2*2
+wynik = str('Pole trójkąta wynosi:  ' +str(t))
+print(wynik)

23410ed5b4ab5d75abdcbb18b00af3904b022608
KFranciszek <krzysztof.zernicki@gmail.com>
2017-01-22 14:13:19 +0100

    Create JakaToLiczba.py

---
+++ JakaToLiczba.py
@@ -0,0 +1,22 @@
+#Zmodyfikuj program Jaka to liczba? tak, aby gracz dysponował ograniczoną
+#liczbą prób odgadnięcia wybranej przez komputer liczby. Gdy graczowi nie uda
+#się odgadnąć tej liczby w wyznaczonej liczbie prób, program powinien
+#wyświetlić odpowiedni komunikat z reprymendą
+
+
+
+licznik = True
+while licznik:
+
+    import random
+    liczba = random.randint(1,100)
+    print ("Zgadnij liczbę w przedziała 1-100")
+    odp = input("Wylosowana liczba to: ")
+    if odp == liczba:
+        print("Prawidłowa odpoiedź, liczba wylosowana to " + liczba)
+    else:
+        print("Źle prawidłowa licza to:" + str(liczba))
+        break
+#########################################
+
+

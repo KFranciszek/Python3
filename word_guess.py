107cb5681a1fe290b3373f217fc9208380a21147
KFranciszek <krzysztof.zernicki@gmail.com>
2017-02-07 23:55:18 +0100

    Create word_guess.py

---
+++ word_guess.py
@@ -0,0 +1,33 @@
+import random
+words = ['cat', 'dog', 'house','love', 'computer']
+print('Zacznijmy grę w zgadywanie słów')
+i=random.choice(words)
+proby = 5
+guese = True
+while True:
+    print("Słowo to ma " , len(i), 'liter')
+    liter=input('Wybierz literkę do podpowiedz')
+    if liter in i:
+       print("Ta litera znajduje się w słwoie")
+       zgadnij=input("Czy chcesz zgadnąć słowo? Tak/Konty")
+       if zgadnij == "Tak":
+           slowo=input("Podaj słowo")
+           if slowo in words:
+               print("Brawo zgadłeś!")
+               break
+       elif zgadnij =="KOnty":
+           continue
+           roby -= 1
+           print("Pozstało ci ", proby)
+
+
+
+    else:
+        print("Litera nie znajduje się w słowniku")
+        proby -= 1
+        print("Pozstało ci ", proby)
+        if proby > 5:
+            break
+            print("koniec gry Przegrałeś")
+
+

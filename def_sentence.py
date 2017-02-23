7fce55d6b96df7b05d50ac082596ab12d9f956f1
KFranciszek <krzysztof.zernicki@gmail.com>
2017-02-23 20:50:01 +0100

    Create def_sentence.py

---
+++ def_sentence.py
@@ -0,0 +1,14 @@
+def word():
+    ask = input("Give sentence")
+    return ask
+    
+def split_word(ask):
+    split_title = ask.title().split()
+    return split_title
+
+def big(split_title):
+    bigt = split_title
+    for element, i in enumerate(bigt):
+        if len(i) < 3:
+            bigt[element] = bigt[element].lower()
+    print(bigt)

15aa0357fd5477b083464b202bcd4033ec77504e
KFranciszek <krzysztof.zernicki@gmail.com>
2017-02-27 21:50:43 +0100

    Create random.py

---
+++ random.py
@@ -0,0 +1,18 @@
+import random
+list =["kot", 'pies', 'kasa', 'kinga', 'jagoda']
+i = random.choice(list)
+print(i)
+
+import random
+kosc = random.randint(1,6)
+print(kosc)
+
+import random
+list =["kot", 'pies', 'kasa', 'kinga', 'jagoda']
+p = random.sample(list,3)
+print(p)
+
+import random
+s = random.randrange(0,12,2)
+print(s)
+

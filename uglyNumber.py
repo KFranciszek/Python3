5f774a9a86405584624e38d588489152280efcc5
KFranciszek <krzysztof.zernicki@gmail.com>
2017-03-02 21:37:53 +0100

    Create uglyNumber.py

---
+++ uglyNumber.py
@@ -0,0 +1,13 @@
+
+def ugly():
+    ask = float(input("Take a number"))
+    if  ask  % 2 ==0:
+        print("Ugly numbera")
+    elif ask % 3 == 0:
+        print("Ugly numbera")
+    elif ask % 5 == 0:
+        print("Ugly numbera")
+    else:
+        print("Good numebr!")
+
+ugly()

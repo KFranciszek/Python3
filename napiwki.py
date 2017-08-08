d48215025ce8be5486de5e9849c6062d910a34a4
KFranciszek <krzysztof.zernicki@gmail.com>
2017-08-08 22:00:29 +0200

    Create napiwki.py

---
+++ napiwki.py
@@ -0,0 +1,5 @@
+print("Witaj w programie kalkulator napiwków")
+R = int(input("Wprowadź kwotę do zapłaty"))
+N1 = 15 *R//100
+N2 = 20 *R//100
+print("Możesz zostawić dwie kwoty napiwku: "  +str(N1) +" lub " + str(N2))

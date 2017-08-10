e697be68a5e0f65634560429d7a47581cfa64905
KFranciszek <krzysztof.zernicki@gmail.com>
2017-08-10 23:45:20 +0200

    Create DateTimeScripts.py

---
+++ DateTimeScripts.py
@@ -0,0 +1,9 @@
+from _datetime import datetime
+now=datetime.now()
+m=now.month
+d =now.day
+print("Obecnie mamy " + str(m)+" miesiąc i  "+
+      str(d)+ " dzień ")
+# EndM=12-m ile do końca miesiacy
+# EndD= ile do końca dni/if na różną długość miesiacy ?
+# HowLongToEnd=

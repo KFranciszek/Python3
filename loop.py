8e4682c4873b3a1bdf35f84e75d515e951ecc26b
KFranciszek <krzysztof.zernicki@gmail.com>
2017-08-13 16:10:35 +0200

    Create loop.py
    Found element on the list and  find his position

---
+++ loop.py
@@ -0,0 +1,9 @@
+skill = ['strong', 'stamina', 'magic', 'fly', 'bow']
+
+for i in skill:
+    if i == "fly":
+        print("Found Your skill: " +i + " -element is on: "+ str(skill.index(i)) + " position in the list")
+
+
+
+

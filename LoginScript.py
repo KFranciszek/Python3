cd5fff73e1e2f7b4b0c3a992bbb5f593829fcf8a
KFranciszek <krzysztof.zernicki@gmail.com>
2017-08-16 22:17:44 +0200

    Update LoginScript.py

--- LoginScript.py
+++ LoginScript.py
@@ -8,6 +8,13 @@ if (len(pasword)) < 6:
     print('Twoje hasło musi zawierać minimum 6 znaków')
 if "!" not in pasword:
     print("Twoje hasło musi zawierać znak specjalny")
+    
+    
+# for i in pasword:
+#     if i is not i.upper()
+#         print("Twoje hasło musi zawierać minimum jedeną dużą litere")    
+    
+    
 # if duża litera
 # if znaki specjalne
 # if liczby 

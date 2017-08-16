f8899257dc960bc5ea0259120f7b8e9daca4188d
KFranciszek <krzysztof.zernicki@gmail.com>
2017-08-16 18:58:53 +0200

    Create LottoGame.py

---
+++ LottoGame.py
@@ -0,0 +1,8 @@
+wynik=[]
+while count:
+    import random
+    lotto =random.randint(1,49)
+    wynik.append(lotto)
+    if len(wynik) >=6:
+        print("Twoje szczęśliwe liczby to: " + str(wynik))
+        break

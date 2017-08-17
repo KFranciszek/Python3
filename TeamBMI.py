a2fb77342d429901dd955660e88fd52a9309c69a
KFranciszek <krzysztof.zernicki@gmail.com>
2017-08-17 21:16:06 +0200

    Update TeamBMI.py

--- TeamBMI.py
+++ TeamBMI.py
@@ -21,3 +21,42 @@ def BMI_print ():
 
 DicBMI_data()
 BMI_print()
+#######
+# Wersja  z zapisaniem do pliku # 
+
+team = []
+def DicBMI_data():
+    member= True
+    while member:
+        name = input('Podaj imie')
+        weight = int(input('Podaj swoją wage'))
+        height = float(input('Podaj swój wzrost'))
+        bmi = weight//(2*height)
+        bmiDic = {'name': name,
+                  'bmi': bmi}
+
+        team.append(bmiDic)
+        pytanie = input('Czy policzyć jeszcze raz  T/N')
+        if pytanie == 'N':
+            member = False
+            return team and bmiDic
+
+
+def BMI_print ():
+
+
+    for i in team:
+        print(i)
+    return i
+
+def writeBMI (team):
+    for i in team:
+        w=open("BMI_TEAM.txt", 'w')
+        w.write(str(team)+'\n')
+        w.close()
+
+
+
+DicBMI_data()
+BMI_print()
+writeBMI (team)

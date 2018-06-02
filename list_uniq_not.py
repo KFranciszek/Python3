f724f391b09a60e0af27d583b5cda0bfcad22e4c
KFranciszek <krzysztof.zernicki@gmail.com>
2018-06-02 08:42:52 +0200

    Update list_uniq_not.py

--- list_uniq_not.py
+++ list_uniq_not.py
@@ -32,3 +32,17 @@ for i in s:
     if s.count(i) > 1:
         print('Słowo uniklane to:', i,'występuje aż', s.count(i), 'razy')
         break
+
+        
+#  Example 4
+w="Ala ma kota, i kot ma ale. Ala ma psa także"
+w=w.replace('.', '')
+w=w.replace(',', '')
+print(w)
+s=w.lower().split()
+print(s)
+w = len(s)
+for i in s:
+    if s.count(i) > 1:
+       e= 'Słowo uniklane to:', i,'występuje aż', s.count(i), 'razy'
+       print(e)
